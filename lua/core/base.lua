d0b50357627fedad8090906035b697db5deea2d1
VictorKPetersen <victorkahlpetersen@gmail.com>
2023-09-21 00:47:49 +0200

    First Config

---
+++ lua/core/base.lua
@@ -0,0 +1,10 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
+vim.cmd("set cursorline")
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
---
+++ lua/core/remap.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>et", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>ef", vim.cmd.NvimTreeFocus)
